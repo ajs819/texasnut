b6edb44f2fda7e60d7238cb008be21ffd272c14d
Alon Shur <alonshur@gmail.com>
2020-12-17 04:39:13 -0500

    features section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,16 @@
 (function($) {
   "use strict";
 
-  // PARALLAX
-  var rellax = new Rellax('.rellax');
+  /*
+   * PARALLAX
+   */
+
+  // align first section top
+  var rellax = new Rellax('.section:first-child .rellax');
   
+  // align other sections center
+  var rellax = new Rellax('.section:not(:first-child) .rellax', {
+    center: true
+  });
+
 })( jQuery );
